7444d90d87d4244951c1e8450a6b2e094f1dbe62
anurse <andrew@andrewnurse.net>
2014-06-16 15:51:02 -0700

    Added helper to get request ID from Owin Context

---
+++ src/NuGet.Services.Platform/Http/OwinExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuGet.Services;
+
+namespace Microsoft.Owin
+{
+    public static class OwinExtensions
+    {
+        public static string GetRequestId(this IOwinContext self)
+        {
+            return self.Get<string>(Constants.RequestIdOwinEnvironmentKey);
+        }
+    }
+}
