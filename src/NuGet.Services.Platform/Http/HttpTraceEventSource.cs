fbc41637b68f6026d157ca03cda5e973ba39f109
Xavier Decoster <xavier.decoster@outlook.com>
2015-05-11 20:20:45 +0200

    Update license header on files.

--- src/NuGet.Services.Platform/Http/HttpTraceEventSource.cs
+++ src/NuGet.Services.Platform/Http/HttpTraceEventSource.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics.Tracing;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+using System.Diagnostics.Tracing;
 using System.Net;
 
 namespace NuGet.Services.Http
